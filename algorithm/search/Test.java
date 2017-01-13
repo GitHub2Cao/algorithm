71e44819848caa66ef8badf05ac8937f14b5d4b2
cgm <plextocao@gmail.com>
2017-01-13 10:59:27 +0800

    TwoThreeTree

--- algorithm/search/Test.java
+++ algorithm/search/Test.java
@@ -1,9 +1,32 @@
 package algorithm.search;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Test {
 	public static void main(String[] args) {
+		Pattern pattern = Pattern.compile("2\\.\\d{1}\\.\\d{1}");
+		Matcher matcher = pattern.matcher("ss2.0.3sdf");
+		while (matcher.find()) {
+			System.out.println(matcher.group());
+		}
+		
+		
+		System.out.println(Arrays.toString("0,1,3,4,5".split(",")));
+	}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 //		BST<String> bst = new BST<String>();
 //		
 //		bst.put("B");
@@ -28,12 +51,9 @@ public class Test {
 		
 		
 		
-		ArrayList<String> arr = new ArrayList<>();
-		
-		arr.toString().replace("[", "").replace("]", "");
 		
 		
 		
 	}
 
-}
+
